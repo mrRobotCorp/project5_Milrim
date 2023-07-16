c870f171ae0ec0ee0e96e17e8b57f3fe6e10d460
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-16 17:34:16 +0900

    .
    .

---
+++ js/header.js
@@ -0,0 +1,23 @@
+const myProfile = document.querySelector(".myProfile");
+const mpList = document.querySelector(".mpList");
+const mypageAll = document.querySelector(".mypageAll");
+const adminAll = document.querySelector(".adminAll");
+
+myProfile.addEventListener("mouseover", function() {
+    mpList.classList.add("active");
+});
+
+myProfile.addEventListener("mouseleave", function() {
+    mpList.classList.remove("active");
+});
+
+const loc = window.location.href;
+
+if(loc.includes("home")) {
+    mypageAll.style.display = "none";
+}
+
+if(loc.includes("admin")) {
+    adminAll.style.display = "flex";
+    myProfile.style.display = "none";
+}
