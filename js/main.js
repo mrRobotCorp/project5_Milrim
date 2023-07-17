ab97714360a617f8814eaf13b9c7b009a2405d47
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-17 12:23:58 +0900

    .
    .

--- js/main.js
+++ js/main.js
@@ -1,3 +1,18 @@
+// ------------------- 요일 별 백그라운드 변경 ----------------
+let date = new Date();
+let day = date.getDay();
+
+const dayList = document.querySelectorAll(".dayList");
+
+for(let i=0; i<7; i++) {
+    if(day == i+1) {
+        dayList[i].style.background = "#ffd5628e";
+    }
+};
+
+
+
+
 $(document).ready(function(){
     $('.slider').slick({
         dots: true, 
