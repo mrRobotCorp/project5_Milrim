5fb965dd139541f2b0dc7768b66cb2deebae868f
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-17 17:51:01 +0900

    .
    .

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const dayList = document.querySelectorAll(".dayList");
 
 for(let i=0; i<7; i++) {
     if(day == i+1) {
-        dayList[i].style.background = "#fffcd6";
+        dayList[i].style.background = "#fff4bb";
         dayList[i].style.border = "none";
     }
 };
