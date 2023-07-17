5621b779d96d603207025ad07d17223a144f0d59
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-17 23:19:04 +0900

    .
    .

--- js/main.js
+++ js/main.js
@@ -5,12 +5,16 @@ let day = date.getDay();
 const dayList = document.querySelectorAll(".dayList");
 
 for(let i=0; i<7; i++) {
-    if(day == i+1) {
+    if (day == i+1) {
         dayList[i].style.background = "#fff4bb";
         dayList[i].style.border = "none";
+    } else if (day == 0) {
+        dayList[6].style.background = "#fff4bb";
+        dayList[6].style.border = "none";
     }
 };
 
+// ----- slider ------
 $(document).ready(function(){
     $('.slider').slick({
         dots: true, 
