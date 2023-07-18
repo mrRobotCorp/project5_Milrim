8ed03c4de80e3fad42994994fa5f3d66f27f418a
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-19 00:13:38 +0900

    .
    .

--- js/videoDetail.js
+++ js/videoDetail.js
@@ -1,37 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => { 
-    // This is the bare minimum JavaScript. You can opt to pass no arguments to setup.
     const player = new Plyr('#player');
-    
-    // Expose
     window.player = player;
-  
-    // Bind event listener
-    function on(selector, type, callback) {
-      document.querySelector(selector).addEventListener(type, callback, false);
+
+    const starBtn = document.querySelector(".starBtn");
+
+    starBtn.onclick = function() {
+        this.classList.toggle("active");
     }
+
   
-    // Play
-    on('.js-play', 'click', () => { 
-      player.play();
-    });
-  
-    // Pause
-    on('.js-pause', 'click', () => { 
-      player.pause();
-    });
-  
-    // Stop
-    on('.js-stop', 'click', () => { 
-      player.stop();
-    });
-  
-    // Rewind
-    on('.js-rewind', 'click', () => { 
-      player.rewind();
-    });
-  
-    // Forward
-    on('.js-forward', 'click', () => { 
-      player.forward();
-    });
+    // ---------- 공유 버튼 (링크 복사) -------------
+    const url = window.document.location.href;
+    const sBtn = document.querySelector(".shareBtn");
+    
+    sBtn.onclick = function() {
+        window.navigator.clipboard.writeText(url).then(() => {
+            alert("복사되었습니다.");
+        });
+    };
+    
   });
