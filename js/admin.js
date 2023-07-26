7b0c6e201ed238480d38a819d028ac3a0aa906b9
Yoon A Kim <100207457+mrRobotCorp@users.noreply.github.com>
2023-07-26 14:05:19 +0900

    .
    .

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,12 @@
+// -------------------- 사이드 탭 --------------------------
 const tabs = document.querySelectorAll(".nav li");
 const href = window.location.href;
 
-if(href.includes("mem")) {
+if (href.includes("mem")) {
     tabs[0].classList.add("active");
     tabs[1].classList.remove("active");
     tabs[2].classList.remove("active");
-} else if(href.includes("vid")) {
+} else if (href.includes("vid")) {
     tabs[0].classList.remove("active");
     tabs[1].classList.add("active");
     tabs[2].classList.remove("active");
@@ -15,10 +16,10 @@ if(href.includes("mem")) {
     tabs[2].classList.add("active");
 }
 
-// ----------------------------------
+// --------------- 전체 선택 ----------------------
 const allCh = document.querySelector("#allCh");
 
-function selectAll(selectAll)  {
+function selectAll(selectAll) {
     const memChBox = document.getElementsByName('memOne');
     
     memChBox.forEach((e) => {
@@ -26,6 +27,29 @@ function selectAll(selectAll)  {
     })
 }
 
-allCh.onclick = function() {
+allCh.onclick = function () {
     selectAll(this);
+}
+
+// ------------------ 페이지네이션, keyup 검색 ------------------------------
+var options = {
+    valueNames: ['memCheck', 'memId', 'memPw', 'name', 'emailAdr', 'pNumber', 'author' ],
+    page: 9,
+    pagination: true
+};
+
+var userList = new List('users', options);
+
+for(let i=1; i<=20; i++) {
+
+    userList.add({
+        memCheck : `<input type="checkbox" name="memOne" id="mem` + i +`" value="">
+        <label for="mem` + i + `"></label>`,
+        memId: "thisisid" + i,
+        memPw : "pww100" + i,
+        name: "회원" + i,
+        emailAdr : "exp" + i + "@naver.com",
+        pNumber : "010-11" + i +"-2222",
+        author : "member"
+    });
 }
